f85449099fe0825332f810bd95696a011f309ffc
Graham <gray@elemental.(none)>
2013-02-23 02:16:54 +0000

    delay timer

--- timer.c
+++ timer.c
@@ -2,15 +2,20 @@
 #include "timer.h"
 
 
+/*
+ * Delay Timer
+ *
+ */
 
 void init_timer(timer* timer)
 {
-  timer->timer = 0;
+  timer->DT = 0;
+  timer->last = SDL_GetTicks();
 }
 
 void set_delay_timer(timer* timer, unsigned char delay)
 {
-  timer->time = delay;
+  timer->DT = delay;
   timer->last = SDL_GetTicks();
 }
 
@@ -22,10 +27,38 @@ unsigned char get_delay_timer(timer* timer)
 void update_timer(timer* timer)
 {
   if (timer->DT > 0) {
-    if ((SDL_GetTicks() - timer->last) > (1000.0f / 60.0f))
+    if ((SDL_GetTicks() - timer->last) > (1000.0f / 60.0f)) {
       timer->DT -= 1;
-    else
       timer->last = SDL_GetTicks();
+    }
   }
+}
 
+
+/*
+ * Sound Timer
+ *
+ */
+
+
+void init_sound_timer(sound_timer* timer)
+{
+  timer->DT = 0;
+  tiemr->last = SDL_GetTicks();
+}
+
+void set_sound_timer(sound_timer* timer, unsigned char sound_for)
+{
+  timer->ST = sound_for;
+  timer->last = SDL_GetTicks();
+}
+
+void update_timer(sound_timer* timer)
+{
+  if (timer->ST > 0) {
+    if ((SDL_GetTicks() - timer->last) > (1000.f / 60.f)) {
+      timer->ST -= 1;
+      timer->last = SDL_GetTicks();
+    }
+  }
 }
--- timer.h
+++ timer.h
@@ -4,17 +4,40 @@
 
 #include "SDL/SDL.h"
 
-struct _timer {
+/*
+ * Delay Timer
+ *
+ */
+
+struct _delay_timer {
   Uint32 DT;  // delay timer register
   Uint32 last;
 };
 
-typedef struct _timer timer;
+typedef struct _delay_timer timer;
 
 
 extern void init_timer(timer*);
 extern void set_delay_timer(timer*, unsigned char);
 extern unsigned char get_delay_timer(timer*);
 extern void update_timer(timer*);
+
+
+/*
+ * Sound Timer
+ *
+ */
+
+
+struct _sound_timer {
+  Uint32 ST;
+  Uint32 last;
+};
+
+typedef struct _sound_timer sound_timer;
+
+extern void init_sound_timer(sound_timer*);
+extern void set_sound_timer(sound_timer*, unsigned char);
+extern void update_sound_timer(sound_timer*);
 
 #endif // TIMER_H
