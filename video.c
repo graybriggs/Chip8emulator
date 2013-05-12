7915931bcfe79abaf9bbaf650322a264188bba50
Graham <grahambriggs@gmail.com>
2013-05-12 22:27:10 +0100

    updated video drawing functions

--- video.c
+++ video.c
@@ -4,45 +4,63 @@
 
 void init_video(video* v)
 {
-	v->screen = SDL_SetVideoMode(VIDEO_W, VIDEO_H, VIDEO_BPP, SDL_SWSURFACE);
-	clear_screen(v);
+  v->screen = SDL_SetVideoMode(VIDEO_W, VIDEO_H, VIDEO_BPP, SDL_SWSURFACE);
+  clear_screen(v);
 }
 
 void clear_screen(video* v)
 {	
-	for (int i = 0; i < SCREEN_H; ++i) {
-		for (int j = 0; j < SCREEN_W; ++j) {
-			virtual_screen[i * SCREEN_H + j] = 0;
-		}
-	}
+  for (int i = 0; i < SCREEN_H; ++i) {
+    for (int j = 0; j < SCREEN_W; ++j) {
+      virtual_screen[i * SCREEN_H + j] = 0;
+    }
+  }
 }
 
 void draw_sprite(video* v, unsigned char x, unsigned char y)
 {
-	SDL_Surface* scr = v->screen;
+  SDL_Surface* scr = v->screen;
 	
-	for (int i = 0; i < y; ++i) {
-		for (int j = 0; j < x; j++) {
+  for (int i = 0; i < y; ++i) {
+    for (int j = 0; j < x; j++) {
 			
-		}
-	}
+    }
+  }
 }
 
-void draw_screen(video* v)
+void draw_chip8_pixel(video* v, unsigned char on_off)
+{
+  SDL_Surface* scr = v->screen;
+
+  for (int h = 0; h < 8; ++h) {
+    for (int w = 0; w < 8; ++w) {
+      if (on_off)
+	(unsigned int*)scr->pixels[h * (scr->pitch / 4) + w] = 0xFF;
+      else
+	(unsigned int*)scr->pixels[h * (scr->pitch / 4) + w] = 0x00;
+    }
+  }
+
+}
+
+int draw_screen(video* v)
 {
   SDL_Surface* scr = v->screen;
 
   if (SDL_MUSTLOCK(scr)) {
     if (SDL_LockSurface(scr) < 0) {
       fprintf(stderr, "Failed locking screen surface\n");
-      return;
+      return -1;
     }
   }
 
-  for (int y = 0; y < 32;  ++y) {
-    for (int x = 0; x < 64 * 8; ++x) {
-      (unsigned int*)scr->pixels[y * (scr->pitch / 4) + x] = 0xFF;
+  for (int y = 0; y < VIDEO_H;  ++y) {
+    for (int x = 0; x < VIDEO_W; ++x) {
+      draw_chip8_pixel(v, v->virtual_screen[y * VIDEO_H + x]);
     }
   }
 
+  if (SDL_MUSTLOCK(scr)) {
+    SDL_UnlockSurface(scr);
+  }
 }
--- video.h
+++ video.h
@@ -9,8 +9,8 @@
 #define VIDEO_BPP 8
 
 struct _video {
-	SDL_Surface* screen;
-	unsigned char virtual_screen[VIDEO_W * VIDEO_H];
+  SDL_Surface* screen;
+  unsigned char virtual_screen[VIDEO_W * VIDEO_H];
 };
 
 typedef struct _video video;
@@ -22,7 +22,10 @@ extern void clear_screen(video*);
 // draws sprite at coordiantes x, y.
 extern void draw_sprite(video*, const unsigned char, const unsigned char, const chip8cpu);
 
-extern char get_sprite_at(char); // for FX29
+extern char get_sprite_at(char); // for instruction FX29
+
+static void draw_chip8_pixel(video*, unsigned char); // called from draw_screen
+extern int draw_screen(video*);
 
 #endif // VIDEO_H
 
